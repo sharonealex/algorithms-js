6e7d9ba325d7a6802a4cb5da1c9d9a09699369ae
Sharon <sharonelizalex@gmail.com>
2021-10-21 14:58:42 +1100

    added backspace function

--- dom-apis/case2/script.js
+++ dom-apis/case2/script.js
@@ -11,22 +11,30 @@ var pEl = document.querySelector("p");
 var outputEls = [h2El, h3El, h4El, h5El, pEl];
 
 //add event listener for keydown
-var writeWords = function(event){
-  var keyPressed = event.key;
-  outputEls.forEach((el)=>{
-     h2El.textContent += keyPressed;
-  }
-
-  )
- 
-
+var writeWords = function (event) {
+    var keyPressed = event.key;
+    if (keyPressed != "Backspace") {
+        outputEls.forEach((el) => {
+            el.textContent += keyPressed;
+        })
+    } else {
+        outputEls.forEach((el) => {
+        var edittedText = el.textContent.slice(0, -1);
+        el.textContent = edittedText;
+        })
+      
+    }
 }
 
 
 //add event listener for clear function
-var clearWords = function(){
-
+var clearWords = function () {
+    outputEls.forEach((el) => {
+        el.textContent = '';
+    })
+    textAreaEl.value = '';
 }
 
-buttonEl.addEventListener('click', clearWords)
-textAreaEl.addEventListener('keydown', writeWords)
+
+buttonEl.addEventListener('click', clearWords);
+textAreaEl.addEventListener('keydown', writeWords);
