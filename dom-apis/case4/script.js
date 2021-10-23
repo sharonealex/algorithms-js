81751378ef0a732257d952bb46553177f7403e1b
Sharon <sharonelizalex@gmail.com>
2021-10-23 23:33:13 +1100

    added js for toggling hidden numbers using data attr

--- dom-apis/case4/script.js
+++ dom-apis/case4/script.js
@@ -1,51 +1,19 @@
-var carousalEl = document.querySelector(".carousal");
-var prevEl = document.querySelector(".prev");
-var nextEl = document.querySelector(".next");
-var index = 0;
-var currentImage;
+var container = document.querySelector(".container");
 
-var images = [
-  "https://picsum.photos/id/1060/536/354?blur=2",
-  "https://picsum.photos/id/870/200/300?grayscale&blur=2",
-  "https://picsum.photos/id/0/5616/3744",
-  "https://picsum.photos/id/10/2500/1667",
-];
-
-carousalEl.style.backgroundImage =
-  "url('https://picsum.photos/id/1060/536/354?blur=2')";
-
-function load() {
-  console.log("loading");
-  carousalEl.setAttribute(
-    "style",
-    "background-image: url('https://picsum.photos/id/1060/536/354?blur=2'); background-position: center; background-repeat: no-repeat; background-size: cover;"
-  );
-}
-
-var navigate = function (direction) {
-  index += direction;
-  if (index > images.length - 1) {
-    index = 0;
+container.addEventListener('click', function(event){
+  event.stopPropagation();
+  console.log(event.target);
+  var element = event.target;
+  if(element.matches('.box'))
+{
+  var state = element.getAttribute("data-state");
+  if (state === "hidden"){
+    element.textContent = element.getAttribute("data-number");
+    element.setAttribute("data-state", "visible")
+  } else {
+    element.textContent = " ";
+    element.setAttribute("data-state", "hidden")
   }
-  if (index < 0) {
-    index = images.length - 1;
-  }
-  currentImage = images[index];
 
-  carousalEl.setAttribute(
-    "style",
-    "background-image: url(" +
-      images[index] +
-      "); background-position: center; background-repeat: no-repeat; background-size: cover;"
-  );
-};
-
-nextEl.addEventListener("click", function () {
-  navigate("1");
-});
-
-prevEl.addEventListener("click", function () {
-  navigate("-1");
-});
-
-load();
+}  
+})
